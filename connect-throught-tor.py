649874bec8c2b8adfce4ff084f8edbbba350f2fd
mhaskar <m.askar@isecur1ty.org>
2016-08-03 02:05:53 +0300

    Create connect-throught-tor.py

---
+++ connect-throught-tor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import socks,urllib2 
+import socket 
+
+def create_connection(address, timeout=None, source_address=None):
+    sock = socks.socksocket()
+    sock.connect(address)
+    return sock
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150) 
+socket.socket = socks.socksocket 
+socket.create_connection = create_connection 
+ip = "http://api.ipify.org/?format=json"
+req = urllib2.urlopen(ip)
+print req.read()
