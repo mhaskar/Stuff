912cc192842e03aa13a7dc030b372ee2fde9fb99
mhaskar <m.askar@isecur1ty.org>
2016-08-03 02:15:24 +0300

    Update connect-throught-tor.py

--- connect-throught-tor.py
+++ connect-throught-tor.py
@@ -11,6 +11,6 @@ def create_connection(address, timeout=None, source_address=None):
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150) 
 socket.socket = socks.socksocket 
 socket.create_connection = create_connection 
-ip = "http://api.ipify.org/?format=json"
+ip = "http://api.ipify.org/?format=text"
 req = urllib2.urlopen(ip)
 print req.read()
